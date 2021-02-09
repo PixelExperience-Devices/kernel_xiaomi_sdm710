c1eba39c21401edf54148afc6b241c179a74c9ab
guoxiao1213 <598464949@qq.com>
2021-02-09 13:08:42 +0800

    techpack/audio: Kill debugging prints

--- techpack/audio/asoc/sdm660-common.c
+++ techpack/audio/asoc/sdm660-common.c
@@ -9,7 +9,6 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
-#define DEBUG
 
 #include <linux/input.h>
 #include <linux/of_gpio.h>
--- techpack/audio/asoc/sdm660-internal.c
+++ techpack/audio/asoc/sdm660-internal.c
@@ -9,7 +9,6 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
-#define DEBUG
 
 #include <linux/of_gpio.h>
 #include <linux/platform_device.h>
